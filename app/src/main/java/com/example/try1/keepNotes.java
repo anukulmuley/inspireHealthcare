feef5a11db694a39bafc37d6f22340d05887482c
anukulmuley <67810586+anukulmuley@users.noreply.github.com>
2020-09-26 15:50:17 +0530

    try1 project ka clone hai...

---
+++ app/src/main/java/com/example/try1/keepNotes.java
@@ -0,0 +1,14 @@
+package com.example.try1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class keepNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keep_notes);
+    }
+}
---
+++ app/src/main/java/com/example/try1/loginAs.java
@@ -0,0 +1,43 @@
+package com.example.try1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class loginAs extends AppCompatActivity {
+    private Button moveToAdmin, moveToStudents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_as);
+
+        moveToAdmin = findViewById(R.id.log_admin);
+        moveToAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToAdminPage();
+            }
+        });
+
+        moveToStudents=findViewById(R.id.log_student);
+        moveToStudents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToStudentPage();
+            }
+        });
+    }
+    private void moveToAdminPage(){
+        Intent intent=new Intent(loginAs.this,doctorSignIn.class);
+        startActivity(intent);
+    }
+
+        private void moveToStudentPage(){
+        Intent intent=new Intent(loginAs.this,studentSignIn.class);
+        startActivity(intent);
+    }
+}
