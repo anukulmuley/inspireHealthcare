368b4c63da663338045d1b0bc5e072eece9fd072
anukulmuley <67810586+anukulmuley@users.noreply.github.com>
2020-09-30 00:12:49 +0530

    try1 project ka clone hai...

---
+++ app/src/main/java/com/example/try1/model.java
@@ -0,0 +1,29 @@
+package com.example.try1;
+
+public class model {
+
+
+    private String date;
+    private String amount;
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+
+
+}
